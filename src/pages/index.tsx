ebe2f98ef50051f65f9fa3ffd2abdd8a677c98a3
swaso <swaso.awijaya@gmail.com>
2025-01-30 11:37:41 +0700

    init project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,26 @@
 import { Button } from "~/components/ui/button";
+import { Moon, Sun } from "lucide-react";
 
 import { api } from "~/utils/api";
+import { useTheme } from "next-themes";
 
 export default function Home() {
   const hello = api.post.hello.useQuery({ text: "from tRPC" });
 
+  // for dark mode
+  const { setTheme } = useTheme();
+
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-background">
+      <main className="flex min-h-screen flex-col items-center justify-center space-y-4 bg-background">
         <h1 className="text-3xl">Qepo cuy</h1>
         <Button>Klik Di mari</Button>
+        <Button onClick={() => setTheme("dark")} size="icon">
+          <Moon />
+        </Button>
+        <Button onClick={() => setTheme("light")} size="icon">
+          <Sun />
+        </Button>
       </main>
     </>
   );
